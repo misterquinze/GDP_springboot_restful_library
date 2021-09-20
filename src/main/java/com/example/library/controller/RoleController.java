e51e13d062c0fbb86a140140c6b9cb20c5992e51
Achmad Alghifari <31951431+misterquinze@users.noreply.github.com>
2021-09-20 12:05:48 +0700

    add role by id service

--- src/main/java/com/example/library/controller/RoleController.java
+++ src/main/java/com/example/library/controller/RoleController.java
@@ -8,6 +8,7 @@ import com.example.library.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ public class RoleController {
     @GetMapping("")
     public List<Role> getAllURole() {
         return roleService.getAll();
+    }
+
+    @GetMapping("/{roleId}")
+    public Role getRoleId(@PathVariable(name="roleId") Long roleId) {
+        return roleService.getRoleById(roleId);
     }
 }
--- src/main/java/com/example/library/service/RoleService.java
+++ src/main/java/com/example/library/service/RoleService.java
@@ -1,12 +1,15 @@
 package com.example.library.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.example.library.entity.Role;
 import com.example.library.repository.RoleRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class RoleService {
@@ -17,7 +20,15 @@ public class RoleService {
         return roleRepository.findAll();
     }
 
+    public Role getRoleById(Long roleId) {
+        Optional<Role> tmpRole = roleRepository.findById(roleId);
 
+        if (tmpRole.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not Found");
+        }
+
+        return tmpRole.get();
+    }
     
     
 }
